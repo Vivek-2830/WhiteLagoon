04f5b56a0a42aec43571e296876f0192436d7d73
Vivek-2830 <164905018+Vivek-2830@users.noreply.github.com>
2024-05-18 12:03:15 +0530

    Yesterday

---
+++ WhiteLagoon.Infrastructure/Migrations/20240517135325_ModfiyNamesInVillaTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WhiteLagoon.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModfiyNamesInVillaTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UpdatedData",
+                table: "Villas",
+                newName: "Updated_Data");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedData",
+                table: "Villas",
+                newName: "Created_Data");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Updated_Data",
+                table: "Villas",
+                newName: "UpdatedData");
+
+            migrationBuilder.RenameColumn(
+                name: "Created_Data",
+                table: "Villas",
+                newName: "CreatedData");
+        }
+    }
+}
