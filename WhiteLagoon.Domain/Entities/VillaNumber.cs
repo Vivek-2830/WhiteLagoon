a77de4c6695ce0602a57af2e2c55fed570c0ba82
Vivek-2830 <164905018+Vivek-2830@users.noreply.github.com>
2024-05-20 16:28:57 +0530

    branch 3 Complete

--- WhiteLagoon.Domain/Entities/VillaNumber.cs
+++ WhiteLagoon.Domain/Entities/VillaNumber.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -11,6 +12,7 @@ namespace WhiteLagoon.Domain.Entities
     public class VillaNumber
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name ="Villa Number")]
         public int Villa_Number { get; set; }
 
         [ForeignKey("Villa")]
