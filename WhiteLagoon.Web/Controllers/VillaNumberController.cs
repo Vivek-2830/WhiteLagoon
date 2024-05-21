d7a63bf8f45502181b919cbd52cf345c23f93ae9
Vivek-2830 <164905018+Vivek-2830@users.noreply.github.com>
2024-05-21 11:34:11 +0530

    branch 10 Complete

--- WhiteLagoon.Web/Controllers/VillaNumberController.cs
+++ WhiteLagoon.Web/Controllers/VillaNumberController.cs
@@ -34,17 +34,31 @@ namespace WhiteLagoon.Web.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(VillaNumber obj)
+        public IActionResult Create(VillaNumberVM obj)
         {
             //ModelState.Remove("Villa");
-            if (ModelState.IsValid)  
+
+            bool roomNumebrExists = _db.VillaNumbers.Any(u => u.Villa_Number == obj.VillaNumber.Villa_Number);
+
+            if (ModelState.IsValid && !roomNumebrExists)  
             {
-                _db.VillaNumbers.Add(obj);
+                _db.VillaNumbers.Add(obj.VillaNumber);
                 _db.SaveChanges();
                 TempData["success"] = "The villa Number has been created successfully"; 
                 return RedirectToAction("Index");
             }
-            return View();
+
+            if(roomNumebrExists)
+            {
+                TempData["error"] = "The villa Number already exists.";
+            }
+            obj.VillaList = _db.Villas.ToList().Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+
+            });
+            return View(obj);
         }
 
         public IActionResult Update(int villaId)
