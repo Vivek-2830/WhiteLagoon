95dbcf6967d19ea3f23ab484e2c247fba2e309b8
Vivek-2830 <164905018+Vivek-2830@users.noreply.github.com>
2024-05-28 12:26:49 +0530

    Package refrence update

--- WhiteLagoon.Infrastructure/obj/Debug/net8.0/WhiteLagoon.Infrastructure.AssemblyInfo.cs
+++ WhiteLagoon.Infrastructure/obj/Debug/net8.0/WhiteLagoon.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhiteLagoon.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+866e4aaa3a45ff8c3a7440de9a281d9bc8d96228")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f68a92424839a22667b800fb193158a748afc6e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhiteLagoon.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WhiteLagoon.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
